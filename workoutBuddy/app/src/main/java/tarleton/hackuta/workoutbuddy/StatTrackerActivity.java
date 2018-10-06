233cfe30943f128a682335de6f41d8552a0a0095
Edwards1314 <36805894+Edwards1314@users.noreply.github.com>
2018-10-06 15:25:51 -0500

    Created Android Project WorkoutBuddy

---
+++ workoutBuddy/app/src/main/java/tarleton/hackuta/workoutbuddy/StatTrackerActivity.java
@@ -0,0 +1,13 @@
+package tarleton.hackuta.workoutbuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StatTrackerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stat_tracker);
+    }
+}
